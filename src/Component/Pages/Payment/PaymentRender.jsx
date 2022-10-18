e9c061b656a53b3aa7f1227709ffcff56d795fa3
Emsun <emsunconcept@gmail.com>
2022-10-18 21:08:08 +0100

    added review successful

--- src/Component/Pages/Payment/PaymentRender.jsx
+++ src/Component/Pages/Payment/PaymentRender.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { Progress } from "../../Progress";
-import { Oderreview } from "./Oderreview";
-import { Payment } from "./Payment";
-import { Shipping } from "./Shipping";
+// import { Oderreview } from "./Oderreview";
+// import { Payment } from "./Payment";
+// import { Shipping } from "./Shipping";
 import "./Shipping.css";
 import "./Review.css";
 import { Buyernav } from "../../Buyernav";
 import "../../../style.css";
 import Footer from "../../Footer";
+import { PaymentProcess } from "./PaymentProcess";
+import { OrderSuccessful } from "./OrderSuccessful";
 
 export const PaymentRender = () => {
 	return (
 		<>
 			<Buyernav />
 			<div class='shipping-container'>
-				<Progress />
+				{/* <Progress />
 				<Shipping />
 				<Payment />
-				<Oderreview />
+				<Oderreview /> */}
+				<Progress />
+				<PaymentProcess />
+				{/* <OrderSuccessful /> */}
 			</div>
 			<Footer />
 		</>
