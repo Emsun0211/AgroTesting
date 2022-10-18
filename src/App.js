03ef3d3c35328a0994934c7b97598e7e020745f1
godswill <godswillchibuzo@gmail.com>
2022-10-18 22:54:23 +0100

    godswill update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import './App.css';
+import { About } from './Component/Pages/About/About';
 import CreateAccount from './Component/Pages/Createaccount/CreateAccount';
-import FAQ from './Component/Pages/FAQ/FAQ';
+// import FAQ from './Component/Pages/FAQ/FAQ';
+
 
 function App() {
   return (
+
     <div className="App">
-      <FAQ />
+      <About/>
+      {/* <FAQ /> */}
       {/* <CreateAccount/> */}
     </div>
   );
--- src/Component/Pages/About/About.jsx
+++ src/Component/Pages/About/About.jsx
@@ -1,5 +1,149 @@
 import React from "react";
+import "./About.css"
 
 export const About = () => {
-	return <div>About</div>;
+	return (
+		<>
+		<main id="the-main">
+       
+	   <div>
+    
+        <div>
+            <div>
+                <div className="searchresults">
+                    <ul id ="searchlist">
+            
+                    </ul>
+                  </div>
+                <h3 className="about-h3">About Us</h3>
+
+                <div>
+                   
+                    <p>Agroease is an online agricultural store and marketplace htmlFor farm input, agricultural equipments, agro processed products etc. you can conveniently and safely shop high quality, fresh and organic agricultural commodities and safely receive your order at your location. On agroease getting value for your money is guaranteed.
+                        You can book product ahead of time, both on large and small quantities. Get freshly harvested products direct from the farm all at very affordable rate.
+                        As an online market place of agricultural product we are entirely committed to providing high quality products, excellent <br/> customer experience and safe transactions to agro-traders, consumers, agro-industries and all agro-users. 
+                        We supply in tonnages and large quantities to exporters of any demanded agro commodities and respond swiftly to bulk orders.
+                        At agroease you get the best of organic produce/products, fresh farm produce, processed agro products, livestock, chemical, fertilizers, seed, livestock feeds, machineries and equipment delivered at your location. We also provide agro services such consultancy, training etc. so whether you are buying or selling agroease is htmlFor you.
+                        Bridging the trust gap between buyers and sellers in the online space, <br/> by ensuring prompt delivery of quality product, as well as safe exchange of money from buyers to sellers through our inbuilt quality check structure is our unique selling proposition and it makes us standout.
+                        At agroease educating our users is a priority. Hence, we provide timely Information, News, Agro learning materials, Articles, and Products update through our Blog. 
+                        It’s not just a product, it is a solution</p>
+                </div> 
+            </div>
+            <div className="mission-and-vision">
+                <div>
+                    <h3 className="about-h3">Our Mission</h3>
+                    <div>
+                        <p>Our vision is to consistently provide an online platform where 
+                            buyers and seller of agro products can seamlessly transact,
+                             without fear of loss, shortage or exploitation. 
+                             Expand use and youth involvement in agriculture in Nigeria
+                            and beyond through the use technology and industrialized structures.</p>
+                    </div>
+                </div>
+                <div>
+                    <h3 className="about-h3">Our Vision</h3>
+                    <div>
+                        <p>Bridge the trust gap between buyers and sellers of agro products in the online space.
+                        Provide 360 degree information, News and Education to agro stakeholders.
+                        Develop and provide earning opportunities through the agroease structure.</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h3 className="about-h3">Why AgroEase</h3>
+                <div>
+                    <ol>
+                                        <li>Convenient and easy marking and purchasing of Agro Products</li>
+                                        <li>Prompt delivery of orders to your location.</li>
+                                        <li>We provide quality products and services at affordable rate</li>
+                                        <li>Sell your product as an agro-merchant or farmer at a go and reach more buyers online and make more profit.</li>
+                                        <li>Buy both large and small quantities</li>
+                                        <li>We provide larger markers and bulk sales to buyers at affordable and subsidized rate</li>
+                                        <li>We provide safe payment methods and ensure both buyers and sellers are satisfied after every transaction.</li>
+                                        <li>We provide safe exchange of money from buyers to sellers through our quality checks and mediating structure</li>
+                               
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h3 className="about-h3" >Meet the Team</h3>
+                <div>
+                    <div className="faculties">
+                        <div className="unit">
+                            <div>
+                                <img src="/assets//nneoma.png" alt=""/>
+                            </div>
+                            <p>Oma Raymond</p>
+                        </div>
+                        <div className="unit">
+                            <div>
+                                <img src="/assets//confidence.jpeg" alt=""/>
+                            </div>
+                            <p>Chidi Confidence</p>
+                        </div>
+                        <div className="unit">
+                            <div>
+                                <img src="/assets//ebuka.png" alt=""/>
+                            </div>
+                            <p>Nwoporo Chukwuebuka</p>
+                        </div>
+                        <div className="unit">
+                            <div>
+                                <img src="/assets//modinat.png" alt=""/>
+                            </div>
+                            <p>Modinat Adenike</p>
+                        </div>
+                    </div>
+                    <div className = "faculties">
+                        <div className="unit">
+                            <div>
+                                <img src="/assets//emeka.png" alt=""/>
+                            </div>
+                            <p>Odezulu Chukuemeka</p>
+                        </div>
+                        <div className="unit">
+                            <div>
+                                <img src="/assets//tola.png" alt=""/>
+                            </div>
+                            <p>Adetola Adeyeye</p>
+                        </div>
+                        <div className= "unit">
+                            <div>
+                                <img src="/assets//susan.png" alt=""/>
+                            </div>
+                            <p>Odii Susan</p>
+                        </div>
+                    </div>
+                    <div  className = "faculties">
+                        <div className= "unit">
+                            <div>
+                                <img src="/assets//godswill.png" alt=""/>
+                            </div>
+                            <p>Godswill Chibuzo</p>
+                        </div>
+                        <div className= "unit">
+                            <div>
+                                <img src="/assets//gbenga.png" alt=""/>
+                            </div>
+                            <p> Akinukawe Gbenga</p>
+                        </div>
+                    </div>
+                    <div  className = "faculties">
+                        <div className= "unit">
+                            <div>
+                                <img src="/assets//cynthia.png" alt=""/>
+                            </div>
+                            <p>Nwankwo Cynthia</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </main> 
+  </>
+	)
+
 };
