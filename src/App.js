9892838fa86270d61c5cada16dfb2a65343628e7
Odezulu Chukwuemeka <odezuluaugustine@gmail.com>
2022-10-19 01:23:19 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 // import './App.css';
 import CreateAccount from './Component/Pages/Createaccount/CreateAccount';
 import FAQ from './Component/Pages/FAQ/FAQ';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* <FAQ /> */}
-      <CreateAccount/>
-    </div>
+    <>
+    <Routes>
+        <Route path="/" element={<Hom />} />
+        <Route path="about" element={<About />} />
+        <Route path="about" element={<FAQ /> } />
+        <Route path="about" element={<CreateAccount />} />
+        {/* <Route path="about" element={<Pay />} /> */}
+      </Routes>
+    </>
   );
 }
 
--- src/Component/Pages/FAQ/FAQ.jsx
+++ src/Component/Pages/FAQ/FAQ.jsx
@@ -6,6 +6,7 @@ import Navbar from "../../Navbar";
 import Footer from "../../Footer";
 
 const FAQ = () => {
+	
 	return (
 		<>
 		<Navbar />
