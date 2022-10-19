718b10d087935839489f20a2f03aa14c3e674269
Emsun <emsunconcept@gmail.com>
2022-10-19 11:35:34 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 // import './App.css';
 // import CreateAccount from './Component/Pages/Createaccount/CreateAccount';
-// import FAQ from './Component/Pages/FAQ/FAQ';
+import FAQ from "./Component/Pages/FAQ/FAQ";
+import CreateAccount from "./Component/Pages/Createaccount/CreateAccount";
+import { Contact } from "./Component/Pages/Contact/Contact";
 import { Routes, Route } from "react-router-dom";
-import { Homepage } from './Component/Pages/Homepage/Homepage';
+import { Homepage } from "./Component/Pages/Homepage/Homepage";
+import { Login } from "./Component/Pages/Login/Login";
+import { AgroEducation } from "./Component/Pages/AgroEducation/AgroEducation";
+
 // import { Payment } from './Component/Pages/Homepage/Payment';
 
 function App() {
-  return (
-    <>
-    <Routes>
-        <Route path="/" element={<Homepage />} />
-        {/* <Route path="About" element={<About />} /> */}
-        {/* <Route path="about" element={<FAQ /> } />
-        <Route path="about" element={<CreateAccount />} />
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Homepage />} />
+				<Route path='Contact' element={<Contact />} />
+				<Route path='Createaccount' element={<CreateAccount />} />
+				<Route path='FAQ' element={<FAQ />} />
+				<Route path='Login' element={<Login />} />
+				<Route path='AgroEducation' element={<AgroEducation />} />
+
+				{/* 
         <Route path="about" element={<Payment />} /> */}
-        {/* <Route path="about" element={< />} />  */}
-      </Routes>
-    </>
-  );
+				{/* <Route path="about" element={< />} />  */}
+			</Routes>
+		</>
+	);
 }
 
 {
-  /* <PaymentRender />; */
+	/* <PaymentRender />; */
 }
 // <div className="App">
 //   <Navbar />
---
+++ src/Component/Pages/AgroEducation/AgroEducation.jsx
@@ -0,0 +1,246 @@
+import React from "react";
+import Footer from "../../Footer";
+import Navbar from "../../Navbar";
+import "./AgroEducation.css";
+
+export const AgroEducation = () => {
+	return (
+		<>
+			<Navbar />
+			<section id='agroeducation-section'>
+				<main>
+					<div className='heading'>
+						{/* <h3>
+							<a href='#' id='agronews'>
+								Agro News
+							</a>
+						</h3> */}
+						{/* <h3>
+							<a href='../agroeducation/agroeducation.html' id='agroeducation'>
+								Agro&nbsp;Education
+							</a>
+						</h3> */}
+					</div>
+					<div className='news-grid'>
+						<div className='body'>
+							<div className='four-news'>
+								<div className='agro-news-main'>
+									<img src='../../assets/cows.png' alt='agro-main' />
+								</div>
+								<div className='agro-news-others'>
+									<div className='news-item'>
+										<div className='product-image'>
+											<img src='/assets/chickens.png' alt='product' />
+										</div>
+										<h3 className='item-title'>All of Confidence people</h3>
+										<div className='item-link'>
+											<h3 className='item-link-heading'>Agro Ease</h3>
+											<div className='item-content'>
+												<p className='item-content-text'>
+													Nneoma our mama of the house and Emeka troublemaker
+													and Ebuka oga problem solver
+												</p>
+												<div className='button-cover'>
+													<i className='uit uit-angle-double-right'>
+														<img
+															className='agronews-angle'
+															src='/assets/Icon.png'
+															alt='icon'
+														/>
+													</i>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div className='news-item'>
+										<div className='product-image'>
+											<img src='/assets/chickens.png' alt='product' />
+										</div>
+										<h3 className='item-title'>All of Confidence people</h3>
+										<div className='item-link'>
+											<h3 className='item-link-heading'>Agro Ease</h3>
+											<div className='item-content'>
+												<p className='item-content-text'>
+													Nneoma our mama of the house and Emeka troublemaker
+													and Ebuka oga problem solver
+												</p>
+												<div className='button-cover'>
+													<i className='uit uit-angle-double-right'>
+														<img
+															className='agronews-angle'
+															src='/assets/Icon.png'
+															alt='icon'
+														/>
+													</i>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className='update'>
+								<h5 id='latest-update-heading'>Latest Update</h5>
+								<div className='updated-news'>
+									<div className='arrow-left'>
+										<i className='uil uil-arrow-right' />
+									</div>
+									<div className='news'>
+										<a className='agronews-links'>
+											Ebere U. on SAYED Day Old Broiler Chicks (Ross 308)
+										</a>
+									</div>
+								</div>
+								<div className='updated-news'>
+									<div className='arrow-left'>
+										<i className='uil uil-arrow-right' />
+									</div>
+									<div className='news'>
+										<a className='agronews-links'>
+											Ebere U. on SAYED Day Old Broiler Chicks (Ross 308)
+										</a>
+									</div>
+								</div>
+								<div className='updated-news'>
+									<div className='arrow-left'>
+										<i className='uil uil-arrow-right' />
+									</div>
+									<div className='news'>
+										<a className='agronews-links'>
+											Ebere U. on SAYED Day Old Broiler Chicks (Ross 308)
+										</a>
+									</div>
+								</div>
+								<div className='updated-news'>
+									<div className='arrow-left'>
+										<i className='uil uil-arrow-right' />
+									</div>
+									<div className='news'>
+										<a className='agronews-links'>
+											Ebere U. on SAYED Day Old Broiler Chicks (Ross 308)
+										</a>
+									</div>
+								</div>
+								<div className='updated-news'>
+									<div className='arrow-left'>
+										<i className='uil uil-arrow-right' />
+									</div>
+									<div className='news'>
+										<a className='agronews-links'>
+											Ebere U. on SAYED Day Old Broiler Chicks (Ross 308)
+										</a>
+									</div>
+								</div>
+								<div className='updated-news'>
+									<div className='arrow-left'>
+										<i className='uil uil-arrow-right' />
+									</div>
+									<div className='news'>
+										<a className='agronews-links'>
+											Ebere U. on SAYED Day Old Broiler Chicks (Ross 308)
+										</a>
+									</div>
+								</div>
+								<div className='updated-news'>
+									<div className='arrow-left'>
+										<i className='uil uil-arrow-right' />
+									</div>
+									<div className='news'>
+										<a className='agronews-links'>
+											Ebere U. on SAYED Day Old Broiler Chicks (Ross 308)
+										</a>
+									</div>
+								</div>
+								<div className='updated-news'>
+									<div className='arrow-left'>
+										<i className='uil uil-arrow-right' />
+									</div>
+									<div className='news'>
+										<a className='agronews-links'>
+											Ebere U. on SAYED Day Old Broiler Chicks (Ross 308)
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className='other-news'>
+							<div className='news-item'>
+								<div className='product-image'>
+									<img src='/assets/chickens.png' alt='product image' />
+								</div>
+								<h3 className='item-title'>All of Confidence people</h3>
+								<div className='item-link'>
+									<h3 className='item-link-heading'>Agro Ease</h3>
+									<div className='item-content'>
+										<p className='item-content-text'>
+											Nneoma our mama of the house and Emeka troublemaker and
+											Ebuka oga problem solver
+										</p>
+										<div className='button-cover'>
+											<i className='uit uit-angle-double-right'>
+												<img
+													className='agronews-angle'
+													src='/assets/Icon.png'
+													alt='icon'
+												/>
+											</i>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className='news-item'>
+								<div className='product-image'>
+									<img src='/assets/chickens.png' alt='product image' />
+								</div>
+								<h3 className='item-title'>All of Confidence people</h3>
+								<div className='item-link'>
+									<h3 className='item-link-heading'>Agro Ease</h3>
+									<div className='item-content'>
+										<p className='item-content-text'>
+											Nneoma our mama of the house and Emeka troublemaker and
+											Ebuka oga problem solver
+										</p>
+										<div className='button-cover'>
+											<i className='uit uit-angle-double-right'>
+												<img
+													className='agronews-angle'
+													src='/assets/Icon.png'
+													alt='icon'
+												/>
+											</i>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className='news-item'>
+								<div className='product-image'>
+									<img src='/assets/chickens.png' alt='product image' />
+								</div>
+								<h3 className='item-title'>All of Confidence people</h3>
+								<div className='item-link'>
+									<h3 className='item-link-heading'>Agro Ease</h3>
+									<div className='item-content'>
+										<p className='item-content-text'>
+											Nneoma our mama of the house and Emeka troublemaker and
+											Ebuka oga problem solver
+										</p>
+										<div className='button-cover'>
+											<i className='uit uit-angle-double-right'>
+												<img
+													className='agronews-angle'
+													src='/assets/Icon.png'
+													alt='icon'
+												/>
+											</i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</main>
+				{/* PAGINATION */}
+			</section>
+			<Footer />
+		</>
+	);
+};
